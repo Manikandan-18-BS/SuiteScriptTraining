29c31f43d697a3624ada0da5865df0b518b2ae66
Manikandan-18-BS <manikandan.balaguru@jobinandjismi.com>
2024-08-09 10:58:51 +0530

    OTP-7502-fix: added the header

--- OTP-7430-GitHubTraining/src/FileCabinet/SuiteScripts/JobinandJismi/OTP-7430-SUITESCRIPTTRAINING/OTP-7502_External_Custom_Record_form_and_actions/jj_sl_external_custom_rec_action_otp7502.js
+++ OTP-7430-GitHubTraining/src/FileCabinet/SuiteScripts/JobinandJismi/OTP-7430-SUITESCRIPTTRAINING/OTP-7502_External_Custom_Record_form_and_actions/jj_sl_external_custom_rec_action_otp7502.js
@@ -12,7 +12,7 @@
  * *************************************************************************************
  * ******************
  * 
- * Author: Jobin and Jismi IT Services
+ * Author: Jobin and Jismi IT Services LLP
  * 
  * Date Created: 01-August-2024
  * 
