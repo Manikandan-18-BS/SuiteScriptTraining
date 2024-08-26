d2c347cd50926e38199e9d8f8187fa7b1d9578cc
Manikandan-18-BS <manikandan.balaguru@jobinandjismi.com>
2024-08-26 12:19:15 +0530

    OTP-7440-fix: Added Header

--- OTP-7430-GitHubTraining/src/FileCabinet/SuiteScripts/JobinandJismi/RAF/jj_rl_invoice_pdf_response_otp7440.js
+++ OTP-7430-GitHubTraining/src/FileCabinet/SuiteScripts/JobinandJismi/RAF/jj_rl_invoice_pdf_response_otp7440.js
@@ -2,6 +2,39 @@
  * @NApiVersion 2.1
  * @NScriptType Restlet
  */
+/***************************************************************************************
+ * *****************
+ * OTP Task
+ * 
+ * OTP-7430:Generate_Invoice_PDF
+ * 
+ * 
+ * *************************************************************************************
+ * ******************
+ * 
+ * Author: Jobin and Jismi IT Services LLP
+ * 
+ * Date Created: 26-August-2024
+ * 
+ * Description: Create a Restlet endpoint for receiving the invoice print via API response. Create a POST API endpoint for generating invoice PDF. Execution model: Create a POST Restlet API endpoint with the following request body:
+    {
+    "recordName": {unique value},
+    "DocumentNo": { id }
+    }
+    When performing the API request from a third-party application/postman. The Restlet code identify the invoice number is present in the Netsuite account or not. If yes return the PDF print in the response lke following format.
+    {
+    "recordName":"invoice", "documentNo: "",
+    "url":""
+    }
+    The url value in the response must be displayed on a page with invoice print, when opening the url in the web browser, the invoice print must appear on the screen without logging into the Netsuite account. If the invoice document number is not present in the Netsuite, send the same response with the url field empty.
+ * 
+ * REVISION HISTORY
+ * 
+ * @version 1.0 OTP-7430:26-August-2024: Created the initial build by JJ0327
+ * 
+ * 
+ * ***************************************************************************************
+ *******************/
 define(['N/https', 'N/record', 'N/render', 'N/search', 'N/url','N/file'],
     /**
  * @param{https} https
@@ -72,7 +105,7 @@ define(['N/https', 'N/record', 'N/render', 'N/search', 'N/url','N/file'],
                     return{
                         "recordName":recName, 
                         "documentNo": doc,
-                        "url":null
+                        "url":""
                     };
                 }
             }
