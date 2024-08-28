bc58fbeef85022b96a50bb23a0546f800ce47130
Manikandan-18-BS <manikandan.balaguru@jobinandjismi.com>
2024-08-28 11:04:32 +0530

    OTP-7430-ci: object Push

--- OTP-7430-GitHubTraining/src/FileCabinet/SuiteScripts/JobinandJismi/RAF/jj_rl_invoice_pdf_response_otp7440.js
+++ OTP-7430-GitHubTraining/src/FileCabinet/SuiteScripts/JobinandJismi/RAF/jj_rl_invoice_pdf_response_otp7440.js
@@ -4,7 +4,7 @@
  */
 /***************************************************************************************
  * *****************
- * OTP Task
+ * Assesment (RAF)
  * 
  * OTP-7430:Generate_Invoice_PDF
  * 
@@ -35,86 +35,87 @@
  * 
  * ***************************************************************************************
  *******************/
-define(['N/https', 'N/record', 'N/render', 'N/search', 'N/url','N/file'],
-    /**
- * @param{https} https
- * @param{record} record
- * @param{render} render
- * @param{search} search
- * @param{url} url
- * @param{file} file
- */
-    (https, record, render, search, url,file) => {
-      
+    define(['N/https', 'N/record', 'N/render', 'N/search', 'N/url','N/file'],
         /**
-         * Defines the function that is executed when a POST request is sent to a RESTlet.
-         * @param {string | Object} requestBody - The HTTP request body; request body is passed as a string when request
-         *     Content-Type is 'text/plain' or parsed into an Object when request Content-Type is 'application/json' (in which case
-         *     the body must be a valid JSON)
-         * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 'text/plain'; returns an
-         *     Object when request Content-Type is 'application/json' or 'application/xml'
-         * @since 2015.2
-         */
-        const post = (requestBody) => {
-            try{
-                let recName = requestBody.recordName;
-                let doc = requestBody.DocumentNo;
-
-                let invSrch = search.create({
-                    type: search.Type.INVOICE,
-                    filters:[['tranid','is',doc], 
-                            'AND', 
-                            ['mainline','is','T']],
-                    columns:['internalid']
-                });
-
-                let invId = '';
-                
-                invSrch.run().each(function(result){
-                    invId = result.getValue('internalid');
-                    return true;
-                });
-
-                if(invId){
-
-                    let invNum = Number(invId);
-
-                    let pdfFile = render.transaction({
-                        entityId: invNum,
-                        printMode: render.PrintMode.PDF
+     * @param{https} https
+     * @param{record} record
+     * @param{render} render
+     * @param{search} search
+     * @param{url} url
+     * @param{file} file
+     */
+        (https, record, render, search, url,file) => {
+          
+            /**
+             * Defines the function that is executed when a POST request is sent to a RESTlet.
+             * @param {string | Object} requestBody - The HTTP request body; request body is passed as a string when request
+             *     Content-Type is 'text/plain' or parsed into an Object when request Content-Type is 'application/json' (in which case
+             *     the body must be a valid JSON)
+             * @returns {string | Object} HTTP response body; returns a string when request Content-Type is 'text/plain'; returns an
+             *     Object when request Content-Type is 'application/json' or 'application/xml'
+             * @since 2015.2
+             */
+            const post = (requestBody) => {
+                try{
+                    let recName = requestBody.recordName;
+                    let doc = requestBody.DocumentNo;
+    
+                    let invSrch = search.create({
+                        type: search.Type.INVOICE,
+                        filters:[['tranid','is',doc], 
+                                'AND', 
+                                ['mainline','is','T']],
+                        columns:['internalid']
                     });
-
-                    pdfFile.folder = -15;
-                    pdfFile.isOnline = true;
-                    let pdfFileID = pdfFile.save();
-
-                    log.debug('PDF',pdfFile);
-                    log.debug('PDF Stored:',pdfFileID);
-
-                    let pdfUrl = file.load({
-                        id: pdfFileID
+    
+                    let invId = '';
+                    
+                    invSrch.run().each(function(result){
+                        invId = result.getValue('internalid');
+                        return true;
                     });
-
-                    return{
-                        "recordName":recName, 
-                        "documentNo": doc,
-                        "url":pdfUrl.url
-                    }; 
+    
+                    if(invId){
+    
+                        let invNum = Number(invId);
+    
+                        let pdfFile = render.transaction({
+                            entityId: invNum,
+                            printMode: render.PrintMode.PDF
+                        });
+    
+                        pdfFile.folder = -15;
+                        pdfFile.isOnline = true;
+                        let pdfFileID = pdfFile.save();
+    
+                        log.debug('PDF',pdfFile);
+                        log.debug('PDF Stored:',pdfFileID);
+    
+                        let pdfUrl = file.load({
+                            id: pdfFileID
+                        });
+    
+                        return{
+                            "recordName":recName, 
+                            "documentNo": doc,
+                            "url":pdfUrl.url
+                        }; 
+                    }
+                    else{
+                        return{
+                            "recordName":recName, 
+                            "documentNo": doc,
+                            "url":""
+                        };
+                    }
                 }
-                else{
-                    return{
-                        "recordName":recName, 
-                        "documentNo": doc,
-                        "url":""
-                    };
+                catch(e){
+                    log.error('Error found:',e.message);
                 }
+    
             }
-            catch(e){
-                log.error('Error found:',e.message);
-            }
-
-        }
-
-        return {post}
-
-    });
+    
+            return {post}
+    
+        });
+    
